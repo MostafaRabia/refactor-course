a54ee2bbd78cc910d346be8eddd1b57f64310dbf
mostafa <mostafarabia64@gmail.com>
2025-12-29 23:39:53 +0200

    add type to getMealExpenses

--- Expense/ExpenseReport.php
+++ Expense/ExpenseReport.php
@@ -61,7 +61,7 @@ class ExpenseReport {
             : " ";
     }
 
-    private function getMealExpenses(mixed $expense, $mealExpenses): mixed
+    private function getMealExpenses(Expense $expense, $mealExpenses): mixed
     {
         if ($expense->type == ExpenseType::DINNER || $expense->type == ExpenseType::BREAKFAST) {
             $mealExpenses += $expense->amount;
